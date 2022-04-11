04f52fad7ab5997f8ee2f2cad7c302a5a851686d
mholodniuk <259228@student.pwr.edu.pl>
2022-04-11 09:53:26 +0200

    add order check

--- src/main/java/Benchmark/OrderCheck.java
+++ src/main/java/Benchmark/OrderCheck.java
@@ -1,5 +1,31 @@
 package Benchmark;
 
+import java.util.ArrayList;
+import Structures.Movie;
+import Sort.BubbleSort;
+
 public class OrderCheck {
-    
+    public static <T extends Comparable<T>> boolean isInCorrectOrder(ArrayList<T> tab) {
+        T previous = tab.get(0);
+        for(T elem: tab) {
+            if(previous.compareTo(elem) > 0) {
+                return false;
+            }
+            previous = elem;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Movie> movies = Movie.readMoviesFromFile("resources/data_1000.csv");
+
+        BubbleSort.sort(movies);
+        boolean git = OrderCheck.isInCorrectOrder(movies);
+
+        if(git)
+            System.out.println("git");
+        else
+            System.out.println("nie git");
+
+    }
 }
---
+++ src/test/java/HeapSortTest.java
@@ -0,0 +1,44 @@
+import Structures.Movie;
+import Sort.HeapSort;
+
+import static org.junit.Assert.assertArrayEquals;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HeapSortTest {
+    
+    @Test
+    public void testMergeSortInt() {
+        ArrayList<Integer> mixed = new ArrayList<>(Arrays.asList(3, 8, 4, 12, 6, 5, 7, 10, 2));
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(2, 3, 4, 5, 6, 7, 8, 10, 12));
+
+        HeapSort.sort(mixed);
+        assertArrayEquals(expected.toArray(), mixed.toArray());
+    }
+
+    @Test
+    public void testMergeSortMovie() {
+        Movie m1 = new Movie("Film 1", 1.0);
+        Movie m2 = new Movie("Film 3", 3.0);
+        Movie m3 = new Movie("Film 5", 5.0);
+        Movie m4 = new Movie("Film 2", 2.0);
+        Movie m5 = new Movie("Film 6", 6.0);
+        Movie m6 = new Movie("Film 4", 4.0);
+        Movie m7 = new Movie("Film 7", 7.0);
+
+        ArrayList<Movie> mixed = new ArrayList<>();
+        ArrayList<Movie> expected = new ArrayList<>();
+        mixed.add(m1); expected.add(m1);
+        mixed.add(m2); expected.add(m4);
+        mixed.add(m3); expected.add(m2);
+        mixed.add(m4); expected.add(m6);
+        mixed.add(m5); expected.add(m3);
+        mixed.add(m7); expected.add(m5);
+        mixed.add(m6); expected.add(m7);
+
+        HeapSort.sort(mixed);
+        assertArrayEquals(expected.toArray(), mixed.toArray());
+    }
+}
